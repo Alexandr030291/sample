af87853f4e1e0817b48f5bee61b02e7899aa190a
Alexandr <smoke_91@list.ru>
2016-09-07 22:27:14 +0300

    Реализовал счетчик, русский plural

--- public/main.js
+++ public/main.js
@@ -1,26 +1,44 @@
 'use strict';
+/**
+* @see http://artsiom.mezin.eu/technofront/
+*/
 
-function onSubmit(form) {
-	let data ={
-		user:  form.elements['user' ].value,
+function onSubmit (form) {
+	let data = {
+		user: form.elements['user'].value,
 		email: form.elements['email'].value
 	};
 
-	
-	let = request('/users', data);
-	
-	if (request==='100'){
+	let result = request('/users', data);
+
+	if (result > -1) {
 		form.hidden = true;
-		helloWord.innerHTML = hello (data.user)
+		let text = hello(data.user)+ ". " + plural(result)
+		window.helloWorld.innerHTML = text;
 	}
 
-	console.log(data,result);
+	console.log(data, result);
 }
 
-function hello (text){
-	return 'Привет, '+ text;
+function hello (text) {
+	return 'Привет, ' + text;
 }
 
-if (typeof exports==='object'){
+if (typeof exports === 'object') {
 	exports.hello = hello;
+}
+
+if (typeof exports === 'object') {
+	exports.plural = plural;
+}
+
+function plural (value){
+	let text = "Вы были на портале " + String(value) +" раз";	
+
+	let i=value;
+	while (i>100) i/=100;
+	i=(i<10||i>20)?i%10:0;
+	text+=(i>1 && i<5)?"a.":".";
+
+	return text;
 }
--- server.js
+++ server.js
@@ -2,6 +2,10 @@ let express = require('express');
 let technologger = require('technologger');
 let parser = require('body-parser');
 let app = express();
+let userslist = {
+	email: [],
+	value: []
+}
 
 app.use('/', express.static('public'));
 
@@ -10,10 +14,22 @@ app.use(technologger);
 
 app.post('/users', (req, res) => {
     console.log(req.body);
-    res.send('100');
     // TODO: вернуть количество обращений
+    res.send(String(getnumber(req.body.email)));
 });
 
 app.listen(process.env.PORT || 3000, () => {
 	console.log(`App started on port ${process.env.PORT || 3000}`);
 });
+
+function getnumber(email){
+	let i = userslist.email.findIndex(str => str === email);
+
+	if (i==-1){
+		i=userslist.email.length;
+		userslist.email.push(email);
+		userslist.value.push(-1);
+	}
+	userslist.value[i]++;
+	return userslist.value[i];
+}
